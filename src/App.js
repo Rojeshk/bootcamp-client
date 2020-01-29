2e02b6002fe5b7b0cd7cd14e8e1865f44f9a5e8e
Karki <Rojesh.Karki@us.sogeti.com>
2020-01-29 14:38:00 -0500

    Initial commit-bootcamp

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import * as React from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
 
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      employees: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3005/')
+      .then(employees => employees.json())
+      .then(employees => {
+        console.log(employees)
+        this.setState({
+          employees: employees
+        })
+
+      })
+      .catch(err => console.log(err))
+  }
+  render() {
+    return (
+
+      <div className="App container">
+        <div  class="media col-10 mb-5 p-3 directory-entry">
+        <h1>Sogeti! Employee Directory</h1></div>
+        <div className="row">
+
+          {this.state.employees.map((employee, index) => {
+            return (
+
+              <div class="media col-14 mb-5 p-5 directory-entry">
+                <img src={employee.image} class="mr-3" alt="..." />
+                
+                <div class="media-body">
+                  <h5 class="mt-0">{employee.name}</h5>
+                  <h6 class="mt-0">{employee.Unit}</h6>
+                  <h6 class="mt-0">{employee.Position}</h6>
+
+                </div>
+              </div>
+              
+            )
+          })
+          }
+        </div>
+      </div>
+      
+    );
+  }
+
+}
 export default App;
